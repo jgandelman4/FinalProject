07a6d48abcc0e33e3befb152f45c49b79ab92bf1
Charlotte Chang <schang@college.harvard.edu>
2012-12-07 21:31:33 -0500

    New login!

--- templates/login_form.php
+++ templates/login_form.php
@@ -15,3 +15,26 @@
     or <a href="registergroup.php">create</a> for a group
     or <a href="register.php">join</a> a group
 </div>
+
+<script>
+$.ajax({
+    url:'https://www.google.com/accounts/ClientLogin',
+    type:'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+    "timeMin": sdatestring,
+    "timeMax": edatestring,
+    "timeZone":"EST",
+    //send master calendar id 
+    "items": [{"id": mastercalendar}]
+    }),
+                            
+    //now we output the free times
+    success:function(response,textStatus,jqXHR){
+        //deal with the string called "Auth"
+        //After a successful authentication request, use the Auth value to create an Authorization header for each request:
+        //Authorization: GoogleLogin auth=yourAuthValue
+
+
+        }
+</script>
